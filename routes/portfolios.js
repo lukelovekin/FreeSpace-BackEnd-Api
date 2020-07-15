1c454500c47cb3a6c05db4f918f5c6ffcd0813b3
Luke <lukelovekin@gmail.com>
2020-07-15 16:08:04 +1000

    heroku deply

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const portfolios = [
+  {name: "hey"}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('get portfolios');
+  res.send(portfolios);
 });
 
 module.exports = router;
