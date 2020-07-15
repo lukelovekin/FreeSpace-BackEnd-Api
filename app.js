bf9eb78ea54052114e6ea2af3cc8454247b4dddb
Luke <lukelovekin@gmail.com>
2020-07-15 13:56:01 +1000

    deploy2

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); // Helps with login messaging
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev')); // morgan is the logger
+app.use(express.json()); // body-parser with new express object
+app.use(express.urlencoded({ extended: false })); // decode the urlencoded data
+app.use(cookieParser()); // process and cookies
+app.use(express.static(path.join(__dirname, 'public'))); // generating path and joing public
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
