a3949f6c5046c61cadd7ccfe65326c72c284dee3
Luke <lukelovekin@gmail.com>
2020-07-20 14:34:54 +1000

    move around and env setup

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var router = express.Router()
+const cors = require('cors')
+const { UsersModel } = require('../models/user')
+
+// can make this less dry
+
+/* GET user listing. */
+router.get('/', async function (req, res) {
+    const portfolios = await PortfolioModel.find()
+    console.log(portfolios)
+    res.send(portfolios)
+})
+
+router.post('/', cors(), async function (req, res) {
+    const { uid, bio } = req.body
+    await PortfolioModel.create({
+        uid,
+        bio
+    })
+        .then(doc => res.send(doc))
+        .catch(err => res.send(err))
+})
+
+
+module.exports = router
