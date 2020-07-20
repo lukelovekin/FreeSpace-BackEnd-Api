9df5833fd12a791d9270b3ed4de68b68c79e6e3c
Luke <lukelovekin@gmail.com>
2020-07-20 14:50:50 +1000

    modularise db

--- util/connectToDb.js
+++ util/connectToDb.js
@@ -15,10 +15,10 @@ function connectToDb() {
     // }
 
     // mongoose.connect("mongodb://localhost/freespace", { useNewUrlParser: true }) //this works with the mongo shell
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:8hZtTwsPNV72ysh3@cluster0.1rjn7.mongodb.net/${process.env.DB_PASS}?retryWrites=true&w=majority`,
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rjn7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
         {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         },
 
         error => {
@@ -29,25 +29,9 @@ function connectToDb() {
                 console.log("Connected to the DB")
             }
         }
+
     )
+
 }
 
 module.exports = connectToDb
-
-
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:8hZtTwsPNV72ysh3@cluster0.1rjn7.mongodb.net/${process.env.DB_PASS}?retryWrites=true&w=majority`,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-
-    error => {
-        if (error) {
-            console.log("There was an Error connecting to the DB")
-            throw error
-        } else {
-            console.log("Connected to the DB")
-        }
-    }
-)
-
