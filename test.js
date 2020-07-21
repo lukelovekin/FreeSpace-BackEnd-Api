d54b7248bc09822134e79221221868332373449d
Luke <lukelovekin@gmail.com>
2020-07-21 15:15:41 +1000

    test draft done

---
+++ test.js
@@ -0,0 +1,64 @@
+const app = require("./app")
+const request = require("supertest")
+const { assert, expect } = require("chai")
+const TestPortfolio = require("./models/portfolio")
+// const mocha = require('mocha')
+
+describe("/portfolios", () => {
+    describe("GET /portfolios", () => {
+        it("should get the portfolios", done => {
+            request(app)
+                .get('/portfolios')
+                .expect(200)
+                .end(function(error, response) {
+                    if (error) return done(error)  
+                        assert(response.body, [])
+                        done()
+                    })
+        })    
+    })
+    describe("GET /portfolios/:id", () => {
+        it("should get a portfolio", done => {
+            request(app)
+                .get('/portfolios/:id')
+                .expect(200)
+                .end(function(error, response) {
+                    if (error) return done(error)
+                    assert(response.body, [])
+                    done()
+                })
+        })
+    })
+    describe("POST /portfolios", () => {
+        it("should save to the database", done => {
+            request(app)
+                .post('/portfolios')
+                .send({"id": 1, "bio": "smelly"})  // change this as we decide on db data
+                .expect(200, done)
+                
+        })
+        it("should not save to the database", done => {
+            request(app)
+                .post("/portfolios")
+                .send({"foo": "bar"})
+                .expect(400, done)
+        })
+    })
+    describe("DELETE /portfolios/:id", () => {
+        it("should delete the portfolio", done => {
+            request(app)
+                .del("/portfolios/1")
+                .expect(200, done)
+        })
+    })
+})
+
+// describe('/users', () => {
+//     describe()
+// })
+
+// before(() => {})
+// after(() => {})
+// after( async () => {
+//     await TestPortfolio.collection.drop
+// })
