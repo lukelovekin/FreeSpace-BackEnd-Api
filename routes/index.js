320a37073e89ab63771f26d81f5c8f592630f37f
Luke <lukelovekin@gmail.com>
2020-07-28 11:33:38 +1000

    bug ficing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { PortfolioModel } = require('../models/portfolio')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('portfolios api testing');
+  res.send('portfolios api testing got to /portfolios for portfolio data');
 });
 
 // router.get('/artist_portal', async function (req, res) {
